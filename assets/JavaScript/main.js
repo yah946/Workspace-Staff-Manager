a41084ebbdab023cdc199520e61991a72fbbd395
Mohamed <mohamedsehrane314@gmail.com>
2025-11-17 07:31:42 +0100

    feat(javascript):regex of all inputs

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -14,26 +14,43 @@ saveBtn.nextElementSibling.addEventListener("click", function () {
 //Add new Experience fields
 newExperience.addEventListener("click", function () {
   const divTag = document.createElement("div");
-  divTag.innerHTML = `<div class="border-t-2 border-indigo-900">
+  divTag.innerHTML = `<div class="mt-2 bg-[#f9f9fb] border border-gray-300 w-full py-1.5 rounded-lg p-2">
             <div>
-              <label for="title-input">Title</label>
-              <input id="title-input" type="text" class="w-full border-2 border-indigo-600 w-full py-1.5 rounded-lg">
-              <div id="title-error"></div>
+              <label for="company-input">Company:</label>
+              <input
+                id="company-input"
+                type="text"
+                class="border border-gray-600 w-full py-1.5 rounded-lg"
+              />
+              <div id="company-input-error"></div>
             </div>
             <div>
-              <label for="date-input">Date</label>
-              <input id="date-input" type="date" class="w-full border-2 border-indigo-600 w-full py-1.5 rounded-lg">
-              <div id="date-error"></div>
+              <label for="role-ex-input">Role:</label>
+              <input
+                id="role-ex-input"
+                type="text"
+                class="border border-gray-600 w-full py-1.5 rounded-lg"
+              />
+              <div id="role-ex-input-error"></div>
             </div>
             <div>
-              <label for="poste-input">poste</label>
-              <input id="poste-input" type="text" class="w-full border-2 border-indigo-600 w-full py-1.5 rounded-lg">
-              <div id="poste-error"></div>
+              <label for="date-f-input">From:</label>
+              <input
+                id="date-f-input"
+                type="date"
+                class="border border-gray-600 w-full py-1.5 rounded-lg"
+              />
+              <div id="date-f-input-error"></div>
             </div>
             <div>
-              <label for="mission-input">Mission</label>
-              <textarea id="mission-input" class="w-full h-16 border-2 border-indigo-600 w-full py-1.5 rounded-lg"></textarea>
-              <div id="mission-error"></div>
+              <label for="date-t-input">To:</label>
+              <input
+                id="date-t-input"
+                type="date"
+                class="border border-gray-600 w-full py-1.5 rounded-lg"
+              />
+              <div id="date-t-input-error"></div>
+            </div>
             </div>`;
   Experiences.appendChild(divTag);
 });
@@ -51,20 +68,40 @@ imageInput.addEventListener("change", function (e) {
   });
   reader.readAsDataURL(fichierSelectionne);
 });
-
 const img = document.getElementById("img");
+
+const inputs = document.querySelectorAll("input");
+let inputsTag = [];
+for (let i = 0; i > inputs.length; i++) {
+  inputsTag.push(inputs[i].classList);
+}
+// console.log(inputsTag[0].includes("hidden"));
+
+// function FindErrors(input) {
+//   inputsTag.forEach((i) => i.classList != hidden);
+// }
+function showErrors() {}
 
 const container = document.getElementById("container");
 const nameInput = document.getElementById("name-input");
 const roleOpt = document.getElementById("role-opt");
 saveBtn.addEventListener("click", function () {
-  container.innerHTML += `<div id="profile" class="flex justify-evenly bg-[#f9f9fb] p-3 w-64 rounded-lg m-2">
+  container.innerHTML += `<div
+          id="profile"
+          class="flex justify-evenly items-center border border-gray-300 bg-[#f9f9fb] p-3 w-64 rounded-lg m-2"
+        >
           <div>
-            <img src="${img.src}" class="border-sky-500 border-2 w-12 h-12 rounded-[50%]">
+            <img
+              src="${img.src}"
+              class="border-[#007afc] border-2 w-12 h-12 rounded-[50%]"
+            />
           </div>
           <div>
             <h2 class="font-bold">${nameInput.value}</h2>
-            <p class="text-gray-400"">${roleOpt.value}</p>
+            <p class="text-gray-400">${roleOpt.value}</p>
+          </div>
+          <div>
+            <button type="button" class="text-[#fac105]">Edit</button>
           </div>
         </div>`;
   modal.classList.add("hidden");
@@ -72,3 +109,37 @@ saveBtn.addEventListener("click", function () {
   roleOpt.value = "";
 });
 //validation
+const patterns = {
+  "name-input": {
+    regex: /^[A-Za-z\s]{2,50}$/,
+    message: "Name must be 2 - 50 caracters long",
+  },
+  "email-input": {
+    regex: /^[A-Za-z0-9._%+-]{2,30}@[A-Za-z.-]{2,10}\.[A-Za-z.]{2,3}$/,
+    message: "Enter a valid email",
+  },
+  "phone-input": {
+    regex: /^\+?[0-9]{7,15}$/,
+    message: "Enter a valid phone number",
+  },
+  "url-input": {
+    regex: /^(http\/\/|https\/\/)?(www\.)?[A-Za-z0-9.-]+\.[A-Za-z]{2,5}$/,
+    message: "Enter a valid url",
+  },
+  "company-input": {
+    regex: /^[A-Za-z\s]{2,50}$/,
+    message: "Company name must be 2 - 50 caracters long",
+  },
+  "role-ex-input": {
+    regex: /^[A-Za-z\s]{2,50}$/,
+    message: "Company name must be 2 - 50 caracters long",
+  },
+  "date-f-input": {
+    regex: /^\d{4}-\d{2}-\d{2}$/,
+    message: "add a date",
+  },
+  "date-t-input": {
+    regex: /^\d{4}-\d{2}-\d{2}$/,
+    message: "add a date",
+  },
+};
